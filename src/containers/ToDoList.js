bd33abd61c1bef7c48f578b158eace3fe5830731
Leigh Larson <leighlarson4@gmail.com>
2020-09-27 01:03:46 -0600

    Add all basic Redux steps from lesson plan

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import ToDo from '../components/ToDo'
+
+const ToDoList = ({todos}) => {
+  const displayTodos = todos.map(todo => {
+    return (
+      <ToDo
+      key={todo.id}
+      {...todo}
+      />
+    )
+  })  
+  return(
+    <ul>
+      {displayTodos}
+    </ul>
+  )
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+})
+
+export default connect(mapStateToProps)(ToDoList)
